292c81f79c8ad1848288d51c95be930b6202cc28
arjun006 <arjun.devakumar66@gmail.com>
2019-01-11 14:00:17 -0500

    setup

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (id, burger_name, devoured) VALUES (1, 'Top Gun', 0);
+INSERT INTO burgers (id, burger_name, devoured) VALUES (2, 'Big Mac', 0);
+INSERT INTO burgers (id, burger_name, devoured) VALUES (3, 'Whopper', 0);
